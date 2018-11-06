8dcf4313dc60a8f93e8f1c7d46ab6eb6102c166d
deepinder <deepinder@whitecircleschool.com>
2018-11-05 22:03:25 -0500

    Diwali Celebrations

--- Assignments/src/day18/Assignment1.java
+++ Assignments/src/day18/Assignment1.java
@@ -27,6 +27,7 @@ public class Assignment1 {
 		//driver.findElement(By.cssSelector("body > div.mCRfo9 > div > div > div > div > div.Km0IJL.col.col-3-5 > div > form > div:nth-child(2) > input")).sendKeys("password");
         // Comment By Indu
 		//New Comment By Deepinder
+		//Diwali is a festival of Lights
 
 		//Learning Git is easier after learning gitbash
 
